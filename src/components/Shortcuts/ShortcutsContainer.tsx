ae9a0a1ff590429970820d0f49baa7a63709312c
wqw <qq@qq.com>
2026-01-11 01:16:12 +0800

    fix: 限制拖拽范围在容器内（基于行列数配置）

--- src/components/Shortcuts/ShortcutsContainer.tsx
+++ src/components/Shortcuts/ShortcutsContainer.tsx
@@ -5,7 +5,7 @@ import {
   useSensor,
   useSensors,
 } from '@dnd-kit/core';
-import { restrictToWindowEdges } from '@dnd-kit/modifiers';
+import { restrictToParentElement } from '@dnd-kit/modifiers';
 import { useState } from 'react';
 import { DraggableItem } from './DraggableItem';
 import { ShortcutCard } from './ShortcutCard';
@@ -102,7 +102,7 @@ export function ShortcutsContainer({
     <div className={`relative ${className}`}>
       <DndContext
         sensors={sensors}
-        modifiers={[restrictToWindowEdges]}
+        modifiers={[restrictToParentElement]}
         onDragStart={handleDragStart}
         onDragMove={handleDragMove}
         onDragEnd={handleDragEnd}
